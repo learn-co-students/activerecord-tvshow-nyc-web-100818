fa84d4df2f9bc70de611269afddf72cc0c827ab5
brewchetta <chett.tiller@gmail.com>
2018-10-19 14:22:38 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    s = self.highest_rating
+    self.where("rating == ?", s)[0]
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    s = self.lowest_rating
+    self.where("rating == ?", s)[0]
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
+
+end
